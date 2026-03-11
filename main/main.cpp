0dc5bd0b78d5e43e06d999ae7464fa2fc3a16ff6
Reynaldo Canas <reynaldocanas@Reynaldos-MacBook-Air-4.local>
2026-03-11 14:40:33 -0400

    rey initial commit

--- main/main.cpp
+++ main/main.cpp
@@ -2,6 +2,7 @@
 #include <driver/gpio.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
+#include <esp_bt.h>
 
 constexpr gpio_num_t LED_PIN = GPIO_NUM_2;
 constexpr uint32_t HIGH = 1;
@@ -18,4 +19,18 @@ extern "C" void app_main(void){
         printf("LED OFF\n");
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
+}
+
+//initialize teh bluetooth controller to allocate task and other resources
+esp_err_t esp_bt_controller_init(const esp_bt_controller_config_t *cfg){
+    return ESP_OK, ESP_ERR_INVALID_STATE, ESP_ERR_INVALID_ARG, ESP_ERR_NO_MEM;
+}
+
+esp_err_t esp_bt_controller_enable(esp_bt_mode_t mode){
+    return ESP_OK, ESP_ERR_INVALID_STATE, ESP_ERR_INVALID_ARG;
+}
+
+//De-initialize Bluetooth Controller to free resources and delete tasks.
+esp_err_t esp_bt_controller_deinit(void){
+    return ESP_OK, ESP_ERR_INVALID_STATE;
 }
