bc60b228cc2d502214ab2857575b003be2ed7d05
gabenogu <fishmangabitan@gmail.com>
2026-03-18 22:59:05 -0400

    deleted unnecessary includes

--- main/main.cpp
+++ main/main.cpp
@@ -1,14 +1,12 @@
-#include <stdio.h>
 #include "mpu_6050.h"
 #include "Bluetooth_WYP.h"
-#include <mutex>
 #include <iostream>
 
 extern "C" void app_main(void){
 
     //Initialize Bluetooth on ESP32
     ble_gap_init();
-    
+
     // Initialize the MPU6050 sensor
     MPU6050 sensor(I2C_NUM_0, 0x68); // Use I2C port 0 and address 0x68
 
