8c3be5338921b8655856eece5c20fa843793f6a4
callmemeomeo <caoanh416@Gmail.com>
2026-03-20 16:07:18 -0400

    work in progress

--- components/MPU6050_Driver_Class/mpu_6050.cpp
+++ components/MPU6050_Driver_Class/mpu_6050.cpp
@@ -1,5 +1,9 @@
 #include "mpu_6050.h"
 
+#define ACCEL_SCALE 16384.0f 
+#define GYRO_SCALE 131.0f
+#define ONE_TIME_DELAY (1000 / portTICK_PERIOD_MS)
+#define CONVERT_TEMP 340.0f + 36.53f
 
 MPU6050::MPU6050(i2c_port_t port, uint8_t address){
     // Initialize the sensor (e.g., wake it up)
@@ -18,9 +22,9 @@ MPU6050::MPU6050(i2c_port_t port, uint8_t address){
     i2c_driver_install(i2c_port, conf.mode, 0, 0,0);
 
     uint8_t wake_cmd[2] = {WAKEUP_REG, 0x00};
-    i2c_master_write_to_device(i2c_port, slave_addr, wake_cmd, sizeof(wake_cmd), 1000 / portTICK_PERIOD_MS); 
+    i2c_master_write_to_device(i2c_port, slave_addr, wake_cmd, sizeof(wake_cmd), ONE_TIME_DELAY); 
     const uint8_t config_register[2] = {0x1C,0x00};
-    i2c_master_write_to_device(i2c_port, slave_addr, config_register, sizeof(config_register),1000 / portTICK_PERIOD_MS);
+    i2c_master_write_to_device(i2c_port, slave_addr, config_register, sizeof(config_register), ONE_TIME_DELAY);
 }
 
 inline int16_t combine(uint8_t high, uint8_t low) {
@@ -32,18 +36,18 @@ void MPU6050:: read(SensorData *data) {
     // Read 14 bytes of data starting from the DATA_START_REG
     uint8_t buffer[14];
     uint8_t reg = DATA_START_REG;
-    i2c_master_write_read_device(i2c_port,slave_addr, &reg, 1, buffer, 14, 1000 / portTICK_PERIOD_MS);
+    i2c_master_write_read_device(i2c_port,slave_addr, &reg, 1, buffer, 14, ONE_TIME_DELAY);
     
     // Convert the raw data to signed integers
-    data->accel.x = combine(buffer[0], buffer[1]) / 16384.0f;
-    data->accel.y = combine(buffer[2], buffer[3])/16384.0f;
-    data->accel.z = combine(buffer[4], buffer[5]) / 16384.0f;
+    data->accel.x = combine(buffer[0], buffer[1]) /(ACCEL_SCALE);
+    data->accel.y = combine(buffer[2], buffer[3])/(ACCEL_SCALE);
+    data->accel.z = combine(buffer[4], buffer[5]) /(ACCEL_SCALE);
 
-    data->temp = combine(buffer[6], buffer[7]) / 340.0f + 36.53f;
+    data->temp = combine(buffer[6], buffer[7]) / (CONVERT_TEMP);
 
-    data->gyro.x = combine(buffer[8], buffer[9]) / 131.0f;
-    data->gyro.y = combine(buffer[10], buffer[11]) / 131.0f;
-    data->gyro.z = combine(buffer[12], buffer[13]) / 131.0f;
+    data->gyro.x = combine(buffer[8], buffer[9]) / (GYRO_SCALE);
+    data->gyro.y = combine(buffer[10], buffer[11]) / (GYRO_SCALE);
+    data->gyro.z = combine(buffer[12], buffer[13]) / (GYRO_SCALE);
 }
 void print_data(MPU6050 &sensor) {
     SensorData data;
