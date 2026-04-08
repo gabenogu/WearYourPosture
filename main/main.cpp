aed27026ff364032b24a319e1155bd223e6d6d8a
Laiba Mahmud <laiba.m@laibas-air.p.fiu.edu>
2026-04-08 03:40:45 -0400

    connected bluetooth state to main loop

--- main/main.cpp
+++ main/main.cpp
@@ -1,9 +1,24 @@
-#include <stdio.h>
 #include "mpu_6050.h"
-#include <mutex>
+#include "Bluetooth_WYP.h"
 #include <iostream>
-#include <esp_bt.h>
 
 extern "C" void app_main(void){
-    set_led();
+
+    //Initialize Bluetooth on ESP32
+    ble_gap_init();
+
+    // Initialize the MPU6050 sensor
+    MPU6050 sensor(I2C_NUM_0, 0x68); // Use I2C port 0 and address 0x68
+
+    while (true) {
+        print_data(sensor);
+        if (ble_conn_handle != BLE_HS_CONN_HANDLE_NONE) {
+            printf("BLE connected\n");
+        } else {
+            printf("BLE not connected\n");
+        }
+        vTaskDelay(1000 / portTICK_PERIOD_MS); // Delay for 1 second
+
+         
+    }
 }
